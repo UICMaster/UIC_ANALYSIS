966a80475340b096cd683dd7b817eebc8488646e
UICMaster <ultrainstinctcrew.gaming@gmail.com>
2026-05-04 20:53:56 +0200

    Create analytics.js

---
+++ src/core/analytics.js
@@ -0,0 +1,11 @@
+function calculateGD15(playerTimeline, enemyTimeline) {
+    // Placeholder: Math for Gold Diff at 15
+    return 0;
+}
+
+function calculateDamageEfficiency(damage, gold) {
+    // Placeholder: Math for Damage/Gold
+    return (damage / gold).toFixed(2);
+}
+
+module.exports = { calculateGD15, calculateDamageEfficiency };
