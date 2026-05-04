284aa272e8525d61843d9dd9444fb09c5349d3e7
UICMaster <ultrainstinctcrew.gaming@gmail.com>
2026-05-04 20:54:42 +0200

    Create messages.js

---
+++ src/discord/messages.js
@@ -0,0 +1,6 @@
+async function updateLeaderboards(teamsData) {
+    // Placeholder: Chunks 60 players into 6 embeds and PATCHES the Discord message
+    console.log("   -> [Discord] Editing Leaderboard messages...");
+}
+
+module.exports = { updateLeaderboards };
