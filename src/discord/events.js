beadecc991ed390bbdf1cbf023cb135e0b429b52
UICMaster <ultrainstinctcrew.gaming@gmail.com>
2026-05-04 20:54:19 +0200

    Create events.js

---
+++ src/discord/events.js
@@ -0,0 +1,6 @@
+async function createOrUpdateEvent(matchData) {
+    // Placeholder: Authenticates as Bot and creates Guild Scheduled Event
+    console.log(`   -> [Discord] Creating event for ${matchData.myTeam}`);
+}
+
+module.exports = { createOrUpdateEvent };
